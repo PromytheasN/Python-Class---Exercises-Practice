2761e17610b4d8aeb53383e52715a500d042a17c
PromytheasN <63561890+PromytheasN@users.noreply.github.com>
2020-05-15 09:31:08 -0700

    Update Class Excersizes.py

--- Class Excersizes.py
+++ Class Excersizes.py
@@ -1,28 +1,31 @@
 #1. Write a Python class to convert an integer to a roman numeral.
 
-class RomanNumeral:
+class Converter:
     
-    def transformation(self, num):
-        integ = [1000, 900, 500, 400,
-                 100, 90, 50, 40,
-                 10, 9, 5, 4,
-                 1]
-        roman = ["M", "CM", "D", "CD",
-                 "C", "XC", "L", "XL",
-                 "X", "IX", "V", "IV",
-                 "I"]
+    def conv_processor(self,num):
         
-        roman_num = ""
+        values = [1000,900,500,400,
+                  100,90,50,40,
+                  10,9,5,4,
+                  1]
+        numerals = ["M","CM","D","CD",
+                    "C","XC","L","XL",
+                    "X","IX","V","IV",
+                    "I"]
+        
         i = 0
+        roman_num =[]
         while num > 0:
-            for x in range(num//integ[i]):
-                num -= integ[i]
-                roman_num += roman[i]
-            i += 1
+            
+            for x in range(num//values[i]):
                 
+                roman_num += numerals[i]
+                num -= values[i]
+            i += 1
         return "".join(roman_num)
+                         
       
       #you can test it by calling the class like this.
-      #print(RomanNumeral().transformation(number))
-      #where number is a random number eg 550
+      #print(Converter().conv_processor(num))
+      #where num can be a random number eg 550
 
